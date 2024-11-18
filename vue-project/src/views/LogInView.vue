7f6f28d152ca72a35361d0b5bd45c1d223d92653
unknown <jhc03032@naver.com>
2024-11-18 20:57:47 +0900

    1

--- vue-project/src/views/LogInView.vue
+++ vue-project/src/views/LogInView.vue
@@ -1,9 +1,26 @@
 <template>
   <div>
+    <h1>LogIn Page</h1>
+    <form>
+      <div>
+        <label for="username">username : </label>
+        <input type="text" id="username" v-model.trim="username">
+      </div><br>
+      <div>
+        <label for="password">password : </label>
+        <input type="password" id="password" v-model.trim="password">
+      </div><br>
+
+      <input type="submit" value="LogIn">
+    </form>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
+const username = ref(null)
+const password = ref(null)
 
 </script>
 
