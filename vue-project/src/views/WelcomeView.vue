cca2dd8a8093af72836e0607dec92eab70299845
Hyunbeen <hyunbin322@gmail.com>
2024-11-19 11:11:19 +0900

    11.19.01

---
+++ vue-project/src/views/WelcomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>MOOV.</h1>
+    <RouterLink :to="{ name: 'SignUpView' }">SingUp</RouterLink> |
+    <RouterLink :to="{ name: 'LogInView' }">LogIn</RouterLink>
+  </div>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<style scoped>
+
+</style>
