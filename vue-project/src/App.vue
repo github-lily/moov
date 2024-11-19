0a8300b6a4a96b07668366f41960f5741f18070a
Hyunbeen <hyunbin322@gmail.com>
2024-11-19 11:11:19 +0900

    11.19.01

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,13 +1,21 @@
 <template>
-  <HeaderNav />
-  <RouterView />
+  <div>
+    <!-- <RouterLink :to="{ name: 'SignUpView' }">SingUp</RouterLink> |
+    <RouterLink :to="{ name: 'LogInView' }">LogIn</RouterLink> -->
+    <!-- <HeaderNav /> -->
+    <WelcomeView />
+    <RouterView />
+
+  </div>
 </template>
 
 <script setup>
 import { RouterView, RouterLink } from 'vue-router'
-import HeaderNav from './components/common/HeaderNav.vue';
+import WelcomeView from './views/WelcomeView.vue';
+// import HeaderNav from './components/common/HeaderNav.vue';
 
 </script>
 
 <style scoped>
+
 </style>
