36856a9aa64fd4df09eaa118fc8a7dbebc49df74
Hyunbeen <hyunbin322@gmail.com>
2024-11-20 16:31:59 +0900

    datafin

--- vue-project/src/components/common/HeaderNav.vue
+++ vue-project/src/components/common/HeaderNav.vue
@@ -1,31 +1,105 @@
 <template>
-    <header>
-      <nav v-if="store.isLogin">
-         로그인 된 상태 
-        <RouterLink :to="{ name: 'MovieView' }">Movies</RouterLink> |
-        <a href="#" @click.prevent="store.logOut">Logout</a>
-      </nav> 
-       <nav v-else> 
-         로그아웃 상태 
+  <div>
+    <div class="container">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary" >
 
-         <RouterLink :to="{ name: 'SignUpView' }">SingUp</RouterLink> |
-        <RouterLink :to="{ name: 'LogInView' }">LogIn</RouterLink>
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">MOOV.</a>
+          
+          <!-- 화면 줄어들 때 -->
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#" >홈</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">내가 찜한 리스트</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" >마이페이지</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" >로그아웃</a>
+              </li>
+            </ul>
+
+            <!-- 검색 창 -->
+            <form class="d-flex" role="search">
+              <input class="movieSearch" placeholder="영화를 검색해보세요!">
+              <button class="btn" type="submit">
+                <i class="fas fa-search"></i>
+              </button>
+            </form>
+          </div>
+
+        </div>
 
       </nav>
-
-      <RouterView />
-
-    </header>
-  </template>
-  
+    </div>
+  </div>
+</template>
 
 <script setup>
-import { RouterView, RouterLink } from 'vue-router'
-import { useAuthStore } from '@/stores/auth';
-
-const store = useAuthStore()
 
 </script>
 
 <style scoped>
+
+/* nav bar */
+body {
+  position: relative;
+}
+
+.contanier {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.navbar {
+  width: 100%;
+  height: 45px;
+  margin: 20px 0px;
+  border-radius: 50px; 
+  padding-left: 50px;
+  padding-right: 50px;
+  background-color: red;
+}
+
+.bg-body-tertiary {
+  background-color: rgb(255, 255, 115) !important; 
+}
+
+.navbar-brand {
+  font-size: 20px;
+  font-family: 'Krona One';
+  font-weight: bold;
+  margin-right: 50px;
+}
+
+.nav-link {
+  font-family: 'Noto Sans KR';
+}
+
+.movieSearch {
+  border: none;
+  border-bottom: 1px solid gray;
+  width: 100%;
+  text-align: center;
+  background-color: transparent;
+  font-family: 'Noto Sans KR';
+}
+
+.movieSearch:focus {
+  border: none ;
+  border-bottom: 1px solid gray;
+  outline: none;
+}
+
 </style>
--- vue-project/src/components/movie/MovieListItem.vue
+++ vue-project/src/components/movie/MovieListItem.vue
@@ -1,19 +1,23 @@
 <template>
   <div>
-    <RouterLink 
-      :to="{ name: 'DetailView', params: { id: movie.id } }">
-      [상세보기]
-    </RouterLink>
-    <hr>
+    <h3>Movie List</h3>
+      <p>제목 : {{ movie.title }}</p>
+      <img :src="poster" alt="영화포스터" style="height:300px">
+      <p>개요 : {{ movie.overview }}</p>
+      <p>장르 : {{ movie.genre }}</p>
+      <p>개봉일 : {{ movie.release_date }}</p>
   </div>
 </template>
 
 <script setup>
 import { RouterLink } from 'vue-router'
 
-defineProps({
-  movie: Object
+const props = defineProps({
+  movie:Object
 })
+const poster = `https://image.tmdb.org/t/p/w500/${props.movie.poster_path}`
+
+
 </script>
 
 <style>
