da02c5d282cc03f6cc96fa190221a88df632fe46
Hyunbeen <hyunbin322@gmail.com>
2024-11-20 16:31:59 +0900

    datafin

--- vue-project/src/components/movie/MovieList.vue
+++ vue-project/src/components/movie/MovieList.vue
@@ -1,14 +1,10 @@
 <template>
   <div class="movie-list">
     <div class="card">      
-      <h3>Movie List</h3>
-      <p>제목 : {{ movie.title }}</p>
-      <img :src="poster" alt="영화포스터" style="height:300px">
-      <p>개요 : {{ movie.overview }}</p>
-      <p>장르 : {{ movie.genre }}</p>
-      <p>개봉일 : {{ movie.release_date }}</p>
       <MovieListItem 
-      :movie="movie"
+      v-for="movie in store.movies"
+        :key="movie.id"
+        :movie="movie"
       />
     </div>
   </div>
@@ -16,11 +12,9 @@
 
 <script setup>
 import MovieListItem from '@/components/movie/MovieListItem.vue'
+import { useMovieStore } from '@/stores/movie';
 
-const props = defineProps({
-  movie:Object
-})
-const poster = `https://image.tmdb.org/t/p/w500/${props.movie.poster_path}`
+const store = useMovieStore()
 
 
 </script>
--- vue-project/src/views/MovieView.vue
+++ vue-project/src/views/MovieView.vue
@@ -1,74 +1,29 @@
 <template>
-
   <div class="container">
-      <nav class="navbar navbar-expand-lg bg-body-tertiary" >
-
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">MOOV.</a>
-          
-          <!-- 화면 줄어들 때 -->
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#" >홈</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">내가 찜한 리스트</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" >마이페이지</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" >로그아웃</a>
-              </li>
-            </ul>
-
-            <!-- 검색 창 -->
-            <form class="d-flex" role="search">
-              <input class="movieSearch" placeholder="영화를 검색해보세요!">
-              <button class="btn" type="submit">
-                <i class="fas fa-search"></i>
-              </button>
-            </form>
-          </div>
-
-        </div>
-
-      </nav>
-      <div class="intro">
-        <div class="intro-left">
-          <p class="introtext">Learn English,<br> Enjoy Movies,<br> Love the Journey!</p>
-          <p class="introtext2">
-            At Moov. we help you learn English through carefully selected movies <br> that match your proficiency level. <br> Start you journey toda
-          </p>
-        </div>
-        <div class="intro-right">
-          <div class="mainmovie"></div>
-          <div class="test"></div>
-        </div>
+    <HeaderNav />
+    <div class="intro">
+      <div class="intro-left">
+        <p class="introtext">Learn English,<br> Enjoy Movies,<br> Love the Journey!</p>
+        <p class="introtext2">
+          At Moov. we help you learn English through carefully selected movies <br> that match your proficiency level. <br> Start you journey toda
+        </p>
       </div>
-      <div class="movielist">
-        <MovieList
-          v-for="movie in store.movies"
-          :key="movie.id"
-          :movie="movie"
-        /> 
+      <div class="intro-right">
+        <div class="mainmovie"></div>
+        <div class="test"></div>
       </div>
-    <RouterView />
-    
+    </div>
+    <div class="movielist">
+      <MovieList/> 
+    </div>
   </div>
-
 </template>
 
 <script setup>
 import { onMounted } from 'vue'
 import MovieList from '@/components/movie/MovieList.vue';
 import { useMovieStore } from '@/stores/movie'
+import HeaderNav from '@/components/common/HeaderNav.vue';
 
 const store = useMovieStore()
 
@@ -77,10 +32,10 @@ onMounted(() => {
   console.log('before getmovies')
   
   store.getMovies()
-
+  
+  console.log(store.movies)
   console.log('end of getmovies')
 
-  console.log(store.movies)
 })
 </script>
 
@@ -98,45 +53,6 @@ body {
     align-items: center;
 }
 
-.navbar {
-  width: 100%;
-  height: 45px;
-  margin: 20px 0px;
-  border-radius: 50px; 
-  padding-left: 50px;
-  padding-right: 50px;
-  background-color: red;
-}
-
-.bg-body-tertiary {
-  background-color: rgb(255, 255, 115) !important; 
-}
-
-.navbar-brand {
-  font-size: 20px;
-  font-family: 'Krona One';
-  font-weight: bold;
-  margin-right: 50px;
-}
-
-.nav-link {
-  font-family: 'Noto Sans KR';
-}
-
-.movieSearch {
-  border: none;
-  border-bottom: 1px solid gray;
-  width: 100%;
-  text-align: center;
-  background-color: transparent;
-  font-family: 'Noto Sans KR';
-}
-
-.movieSearch:focus {
-  border: none ;
-  border-bottom: 1px solid gray;
-  outline: none;
-}
 
 /* --------------------------------------------------------- */
 /* nav bar 하단 */
