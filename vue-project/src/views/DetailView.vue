4495311fb9c2c933a8c4347a8bf1a4c5832c2bf8
Hyunbeen <hyunbin322@gmail.com>
2024-11-21 17:48:51 +0900

    ssaftminend

--- vue-project/src/views/DetailView.vue
+++ vue-project/src/views/DetailView.vue
@@ -1,26 +1,31 @@
 <template>
   <div class="container">
-    <HeaderNav/>
+    <HeaderNav />
+
     <div class="movie-container">
-      <div class="movie-poster"></div>
+      <div class="movie-poster">
+        <img v-if="moviePoster" :src="moviePoster" alt="Movie Poster">
+        <p v-else>Loading...</p>
+      </div>
+
       <div class="movie-contents"></div>
     </div>
 
-    <p class="comment-title">Comments</p>
     <div class="comments-container">
-      <div class="comments"></div>
+      <p class="comment-title">Comments</p>
+      <div class="comments-list">
+        <div class="comments"></div>
+      </div>
     </div>
   </div>
-
 </template>
 
 <script setup>
 import axios from 'axios'
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, computed } from 'vue'
 import { useMovieStore } from '@/stores/movie'
 import { useRoute } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
-import MovieComment from '@/components/movie/MovieComment.vue'
 import HeaderNav from '@/components/common/HeaderNav.vue'
 
 const store = useMovieStore()
@@ -28,34 +33,36 @@ const authStore = useAuthStore()
 const route = useRoute()
 const movie = ref(null)
 
+const moviePoster = computed(() => {
+  return movie.value && movie.value.poster_path
+    ? `https://image.tmdb.org/t/p/w500/${movie.value.poster_path}`
+    : null
+})
 
-// DetailView가 마운트되기전에 DRF로 단일 게시글 조회를 요청 후 응답데이터를 저장
 onMounted(() => {
   axios({
     method: 'get',
     url: `${store.API_URL}/api/v1/movies/${route.params.id}/`,
     headers: {
-      Authorization:`Token ${authStore.token}`
+      Authorization: `Token ${authStore.token}`
     }
   })
     .then((res) => {
-      // console.log(res.data)
       movie.value = res.data
     })
     .catch((err) => {
       console.log(err)
     })
 })
-
-const poster = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
-
 </script>
 
 <style scoped>
 .movie-container {
   height: 500px;
   width: 100%;
-  background-color: white;
+  /* background-color: white; */
+  border: 1px solid white;
+  border-radius: 50px;
   display: flex;
   justify-content: space-between;
   align-items: stretch;
@@ -64,7 +71,14 @@ const poster = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
 .movie-poster {
   height: 100%;
   width: 50%;
-  background-color: rgb(180, 180, 180);
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+img {
+  height: 100%;
 }
 
 .movie-contents {
@@ -73,12 +87,17 @@ const poster = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
   background-color: rgb(126, 126, 126);
 }
 
-
-/* commnet */
+/* comment */
 .comment-title {
   margin: 20px 0;
   font-family: 'Krona One';
   font-size: 20px;
   color: white;
+}
+
+.comments-list {
+  width: 100%;
+  background-color: #7c7c7c;
+  height: 100vh;
 }
 </style>
