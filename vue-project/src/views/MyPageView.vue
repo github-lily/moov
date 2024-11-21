0c9d43ea0bacdefae3e99e898e3e26628f413fda
99minj0731 <99minj0731@naver.com>
2024-11-21 23:12:27 +0900

    min

---
+++ vue-project/src/views/MyPageView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <HeaderNav/>
+    <h1 style="color: white;">마이페이지</h1>
+  </div>
+</template>
+
+<script setup>
+import HeaderNav from '@/components/common/HeaderNav.vue';
+
+</script>
+
+<style scoped>
+
+</style>
