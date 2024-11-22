a9136568077f8f786d8c9bc28ee90fa8453d41fd
이현희 <jhc03032@naver.com>
2024-11-22 17:29:00 +0900

    ssafyhyun

--- django-pjt/movies/urls.py
+++ django-pjt/movies/urls.py
@@ -7,16 +7,14 @@ urlpatterns = [
     path('movies/getdatas/', views.getdatas),
     # 영화 목록 조회
     path('movies/', views.movie_list),
-    # 영화 상세 조회 
+    # 영화 상세 조회
     path('movies/<int:movie_pk>/', views.movie_detail),
-    # 영화 댓글 상세 조회
-    path('movies/<int:movie_pk>/comments/<int:comment_pk>/', views.comment_detail),
     # 영화 댓글 생성
-    path('movies/<int:movie_pk>/comments', views.comment_create),
-    # 전체 영화 댓글 조회
-    path('movies/comments', views.comment_list),
+    path('movies/<int:movie_pk>/comments/', views.comment_create),
+    # 영화 댓글 조회&수정&삭제
+    path('movies/<int:movie_pk>/comments/<int:comment_pk>/', views.comment_detail),
     # 사용자가 댓글 단 영화 목록 조회
-    path('movies/user/comments/', views.user_commented_movies, name='user_commented_movies'),
+    path('movies/<str:username>/comments/', views.user_commented_movies),
     # 영화 좋아요 누른 사람 조회
     path('movies/<int:movie_pk>/likemovies', views.like_movies),
     # 좋아요 누른 영화 목록 조회
