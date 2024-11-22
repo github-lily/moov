e3a6e54db191413409bd75ef6ad3add0d56b6fba
Hyunbeen <hyunbin322@gmail.com>
2024-11-22 15:39:57 +0900

    dmdm

--- vue-project/src/components/movie/MovieComment.vue
+++ vue-project/src/components/movie/MovieComment.vue
@@ -1,37 +1,44 @@
 <template>
   <div class="comments-section">
-    <h3>Comments</h3>
+    <div class="comments-create">
+      <textarea v-model="newComment" placeholder="  댓글을 달아보세요!"></textarea>
+      <button @click="addComment">등록</button>
+    </div>
     <ul>
       <li v-for="comment in comments" :key="comment.id">
+        {{ comment }}
         <p><strong>{{ comment.username }}</strong>: {{ comment.content }}</p>
-        <div v-if="authStore.user.id === comment.user">
+        <div v-if="username === comment.username">
           <button @click="editComment(comment)">Edit</button>
           <button @click="deleteComment(comment.id)">Delete</button>
         </div>
       </li>
     </ul>
-    <div>
-      <textarea v-model="newComment" placeholder="Write a comment..."></textarea>
-      <button @click="addComment">Post Comment</button>
-    </div>
   </div>
 </template>
 
-<script setup>
+<script setup> 
 import { ref, onMounted } from 'vue'
 import { useAuthStore } from '@/stores/auth'
 import { useMovieStore } from '@/stores/movie'
+import { defineProps } from 'vue';
+import { useUserStore } from '@/stores/user';
 
 const props = defineProps({
-  movieId : Number
+  movieId:Number
 })
 
+const userStore = useUserStore()
 const authStore = useAuthStore()
 const movieStore = useMovieStore()
 const movieId = props.movieId // 현재 영화 ID를 경로에서 가져옴
 const comments = ref([])
 const newComment = ref('')
-console.log('comment component',props.movieId)
+const commentId = ref(0)
+// console.log('comment component',props.movieId)
+
+const user = userStore.getUser()
+console.log('comment', user)
 
 // 댓글 가져오기
 const fetchComments = async () => {
@@ -50,6 +57,7 @@ const addComment = async () => {
     await movieStore.addComment(movieId, newComment.value) // movieId 포함
     comments.value = movieStore.comments // store에서 업데이트된 comments를 가져옴
     newComment.value = ''
+    commentId.value = id++
   } catch (error) {
     console.error(error)
   }
@@ -71,9 +79,44 @@ onMounted(() => {
 </script>
 
 <style scoped>
-.comments-section {
+.comments-create {
   padding: 20px;
-  background-color: #fff;
   border-radius: 10px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+}
+textarea {
+  width: 100%;
+  height: 40px;
+  margin-right: 10px;
+  font-size: 16px;
+  /* border-radius: 5px; */
+  border: none;
+  border-bottom: 1px solid #ccc;
+  background-color: transparent;
+  display: block;
+  box-sizing: border-box;
+  color: white;
+}
+
+textarea::placeholder {
+  font-size: 16px;
+  color: #888; 
+  font-family: 'Noto Sans KR' ;
+}
+
+textarea:focus {
+  border: none;
+  outline: none;
+  border-bottom: 1px solid #ccc;
+}
+
+button {
+  width: 10%;
+  font-family: 'Noto Sans KR';
+  border: none;
+  border-radius: 50px;
+  background-color: #fff;
 }
 </style>
